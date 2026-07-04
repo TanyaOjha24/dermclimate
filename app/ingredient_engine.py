c5959d58963b033a5bc1cbbd5ac219ec567363fc
TanyaOjha24 <ojhatanyanaresh@gmail.com>
2026-07-04 14:39:24 -0400

    feat: add ingredient engine with Open Beauty Facts API

---
+++ app/ingredient_engine.py
@@ -0,0 +1,16 @@
+# Search Open Beauty Facts API for a product and return its ingredient list
+import requests
+
+def get_product_ingredients(product_name):
+    params = {
+        'search_terms' : product_name,
+        'search_simple' : 1,
+        'action' : 'process',
+        'json' : 1
+    }
+    response = requests.get('https://world.openbeautyfacts.org/cgi/search.pl', params=params)
+    data = response.json()
+    if not data['products'] or not data['products'][0].get('ingredients_text_en'):
+        return None
+    product = data['products'][0]['ingredients_text_en']
+    return product
